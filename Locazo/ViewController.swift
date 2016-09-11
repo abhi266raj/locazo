376d1724b919eced3214dcc0836658cfc1b0eca6
Abhiraj Kumar <abhiraj.kumar@tiladmins-macbook-pro-5.local>
2016-09-11 20:17:24 +0530

    Initial Commit

---
+++ Locazo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locazo
+//
+//  Created by Abhiraj Kumar on 9/11/16.
+//  Copyright © 2016 Locazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
